d0dbc1aea0d71c920bd4b647137ae9e95de8dcd0
CamiloWG <millos2423@gmail.com>
2025-03-16 19:10:19 -0500

    Tailwind + menu superior

--- ecommerce-frontend/src/app/core/components/header-menu/header-menu.component.ts
+++ ecommerce-frontend/src/app/core/components/header-menu/header-menu.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation, HostBinding } from "@angular/core";
+import { Router } from "@angular/router";
+import { Search } from "../Search/Search.component";
+import { BuildingBlocksSegmentedButt } from "../BuildingBlocksSegmentedButt/BuildingBlocksSegmentedButt.component";
 
 @Component({
   selector: 'header-menu',
-  imports: [],
+  imports: [
+    Search,
+    BuildingBlocksSegmentedButt],
   templateUrl: './header-menu.component.html',
   styleUrl: './header-menu.component.scss'
 })
 export class HeaderMenuComponent {
+  @HostBinding("style.display") display = "contents";
 
+  constructor(private router: Router) {}
+
+  onHomeIconClick() {
+    this.router.navigate(["/inicio"]);
+  }
+
+  onUserIconClick() {
+    // Please sync "Login" to the project
+  }
+
+  onShoppingCartIconClick() {
+    // Please sync "Carrito" to the project
+  }
 }
--- ecommerce-frontend/src/app/pages/home/home.component.ts
+++ ecommerce-frontend/src/app/pages/home/home.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
-import { FooterComponent } from '../../core/components/footer/footer.component';
 import { HeaderMenuComponent } from '../../core/components/header-menu/header-menu.component';
 
 @Component({
   selector: 'app-home',
   imports: [
-    FooterComponent,
     HeaderMenuComponent
   ],
   templateUrl: './home.component.html',
