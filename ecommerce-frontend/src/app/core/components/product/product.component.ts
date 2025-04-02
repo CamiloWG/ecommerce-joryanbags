ca0c7355030cb1d67fcce43884164f183782f7ae
CamiloWG <millos2423@gmail.com>
2025-04-01 19:58:44 -0500

    Agregada popup al agregar producto al carrito

--- ecommerce-frontend/src/app/core/components/product/product.component.ts
+++ ecommerce-frontend/src/app/core/components/product/product.component.ts
@@ -1,5 +1,7 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-product',
@@ -17,9 +19,29 @@ export class ProductComponent {
 
   @Output() buttonAddEvent = new EventEmitter<number>();
 
+  constructor(private router: Router) {}
+
   cantidad: number = 1;
 
   addButtonClick() {
     this.buttonAddEvent.emit(this.cantidad);
+    Swal.fire({
+      title: "¡Exito!",
+      text: `Tu ${this.titulo} fue agregado exitosamente al carrito`,
+      icon: 'success',
+      showCancelButton: true,
+      cancelButtonText: "Ir al carrito",
+      confirmButtonText: "Seguir comprando",
+      customClass: {
+        title: "font-sans",
+        popup: "font-sans"
+      }
+    }).then((result) => {
+      if (result.isConfirmed) {        
+        this.router.navigate(['/catalogo']);
+      } else if (result.dismiss === Swal.DismissReason.cancel) {        
+        this.router.navigate(['/carrito']);
+      }
+  });
   }
 }
