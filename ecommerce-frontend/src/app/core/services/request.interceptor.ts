16b162b7a887fabbf1458faeda1363b582984195
CamiloWG <millos2423@gmail.com>
2025-04-08 23:49:10 -0500

    Interceptor debug

--- ecommerce-frontend/src/app/core/services/request.interceptor.ts
+++ ecommerce-frontend/src/app/core/services/request.interceptor.ts
@@ -3,7 +3,7 @@ import { Observable } from 'rxjs';
 
 export const requestInterceptor: HttpInterceptorFn = (req, next) => {
   const token = localStorage.getItem('token');
-
+  console.log('INTERCEPTOR EJECUTADO, TOKEN:', token);
   if (token) {
     const clonedReq = req.clone({
       headers: req.headers.set('Authorization', `Bearer ${token}`),
