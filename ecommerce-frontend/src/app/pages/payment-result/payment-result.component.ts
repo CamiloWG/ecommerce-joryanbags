f5b0c8a054ea1bfeadbb72f16a0f33873efc1b10
CamiloWG <millos2423@gmail.com>
2025-04-20 21:35:10 -0500

    Payment result de acuerdo al estado del pago

--- ecommerce-frontend/src/app/pages/payment-result/payment-result.component.ts
+++ ecommerce-frontend/src/app/pages/payment-result/payment-result.component.ts
@@ -3,6 +3,8 @@ import { HeaderMenuComponent } from '../../core/components/shared/header-menu/he
 import { LogoComponent } from '../../core/components/shared/logo/logo.component';
 import { FooterComponent } from '../../core/components/shared/footer/footer.component';
 import { Route } from '../../core/components/shared/Route/Route.component';
+import { ActivatedRoute } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-payment-result',
@@ -10,11 +12,21 @@ import { Route } from '../../core/components/shared/Route/Route.component';
     HeaderMenuComponent,
     LogoComponent,
     FooterComponent,
-    Route
+    Route,
+    CommonModule
   ],
   templateUrl: './payment-result.component.html',
   styleUrl: './payment-result.component.css'
 })
 export class PaymentResultComponent {
+  isApproved: boolean | null = null;
 
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe(params => {
+      const status = params['bold-tx-status'];
+      this.isApproved = status === 'approved';
+    });
+  }
 }
