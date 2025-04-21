b1be78e7c77c3a4c959e62777d85589cf802fe12
CamiloWG <millos2423@gmail.com>
2025-04-21 18:14:32 -0500

    Ajuste environment nueva URL

--- ecommerce-frontend/src/environments/environment.ts
+++ ecommerce-frontend/src/environments/environment.ts
@@ -3,5 +3,5 @@
 export const environment = {
     production: false,
     API_URL_BASE: 'https://joryanbags.azurewebsites.net',
-    URL_APP: 'https://salmon-sky-0b21d8d0f.6.azurestaticapps.net'
+    URL_APP: 'https://joryan.store'
 }
